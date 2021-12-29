0e01f92839c818df28a4d243640a1c175c4af1ec
Leonardo Silva <leocardosodev@gmail.com>
2021-12-29 12:47:39 -0300

    feat: Welcome Page Completed

---
+++ lib/src/global_widgets/circle_button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CircleButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String iconPath;
+  CircleButton({@required this.onPressed, @required this.iconPath});
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      padding: EdgeInsets.zero,
+      onPressed: this.onPressed,
+      child: SvgPicture.asset(iconPath, width: 40),
+    );
+  }
+}
